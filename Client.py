59b65c349632b4aa9eb9d25967f5f2bdff973af1
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-12-20 22:54:09 +0300

    first study

--- Client.py
+++ Client.py
@@ -9,9 +9,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # sent = 0
     # while sent < len(request):
     #     sent = sent + s.send(request[sent:])  # Send a portion of 'request', starting from 'sent' byte
-    s.send(b"GET / HTTP/1.1\r\nHOST\r\n\r\n")
-    # s.sendall(b"Hello, world")
+   # s.send(b"GET / HTTP/1.1\r\nHOST\r\n\r\n")
+    s.sendall(b"/add?name=activityname")
     # sock.send(b"GET / HTTP/1.1\r\nHost:www.example.com\r\n\r\n")
     data = s.recv(1024)
 
+
+    # GET 
+    # DELETE 
+    # POST
 print(f"Received {data!r}")
--- roomServer.py
+++ roomServer.py
@@ -3,7 +3,9 @@ import socket
 HOST = "127.0.0.1"
 PORT = 9999
 # with command, we don't need to use try catch blocks
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+
     s.bind((HOST, PORT)) 
     s.listen() # if you put any number in this function ex: 5, it means that it will listen 5 connections after that it is going to throw an error
     conn, addr = s.accept() #connection object will be stored in conn, list of ip addresses will be storedin addr
@@ -29,6 +31,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print("fucntType", funcType,"\nname", name)
 
 
+
+            if funcType == "/add":
+                print("add")
+                            
+
             # Unlike send(), this method continues to send data from bytes until either all data has been sent or an error occurs.
             # None is returned on success.
             conn.sendall(data)
+
+
+
+        # /add?name=activityname
+        # /remove?name=activityname: 
+        # /check?name=activityname
