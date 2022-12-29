f7f243f9183d80d9fd1deeac2a32b0d17b3fe8d6
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-12-29 12:18:13 +0300

    add web request and /favicon.ico check operation

--- roomServer.py
+++ roomServer.py
@@ -1,9 +1,12 @@
 import socket
 import os.path
 
+from splitOperations import splitURL
+from splitOperations import responseMessageFormat
+
 HOST = "localhost"
 PORT = 8081
-# with command, we don't need to use try catch blocks
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
 
@@ -15,19 +18,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print(f"Connected by {addr}")
            
-
             data = conn.recv(1024) # take the first 1024 byte, other than that can be junk
+
             if not data:
                 break
             url= data.decode("utf-8")
             print("The url is", url)
-
-            funcType= url.split("?")[0]
-            print("The functype is", funcType)
-
-            name=url.split("?")[1].split("=")[1] # [0] = name , [1] = roomname&day , [2] = 8
-            print("The name is", name)
-
+          
+            funcType,name=splitURL(url)           
 
             if funcType == "/add":
                 isExists = 0
@@ -35,9 +33,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     lines = []
                     file = open("rooms.txt", 'r')                      
                     for line in file:
-                        if(name == line.strip()):
-                            #print("The room name is already exist 403 atilcak")
-                            conn.sendall(b"HTTP/1.1 403 Forbidden\n")
+                        if(name == line.strip()):                                                        
+                            response=responseMessageFormat(b"400 Bad Request",b"Room name is already exist")              
+                            conn.sendall(response)
                             isExists = 1
                             file.close()
                             break
@@ -46,11 +44,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     file = open("rooms.txt", 'a')                   
                     file.write(name)
                     file.write("\n")
-                    #print("The room name is added to the file")
-                    conn.sendall(
-                        b"HTTP/1.1 200 OK\n" +
-                        b"Content-Type: text/html\n" +
-                        b"\n")
+
+                    response =responseMessageFormat(b"200 OK",b"Room name is added")    
+                    conn.sendall(response)
+
                     file.close()
             elif funcType == "/remove":
                 if os.path.exists('rooms.txt'):
@@ -62,8 +59,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     for line in lines:
                         print(line)
                     if not lines.__contains__(name):
-                        #print("The room name is  not exist")
-                        conn.sendall(b"HTTP/1.1 404 Not Found\n")
+
+                        response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                        conn.sendall(response)
                     else:
                         print("The room name is  exist")                                        
                         file = open("rooms.txt", 'w')
@@ -73,16 +71,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             file.write(line)
                             file.write("\n")
                         file.close()
-                    #    with open("ids.txt", "r") as f:
-                    #    lines = f.readlines()   
-                    #    with open("ids.txt", "w") as f:
-                    #    for line in lines:
-                    #        if line.strip("\n") != "hasan":
-                    #            f.write(line)
-                    #
+
+                        response =responseMessageFormat(b"200 OK",b"Room name is removed")             
+                        conn.sendall(response)
                 else:
-                    #print("file not exists")
-                    conn.sendall(b"HTTP/1.1 404 Not Found\n")
+                    response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                    conn.sendall(response)
             elif funcType == "/reserve":
                 if os.path.exists('rooms.txt'):    
                     roomname = name.split("&")[0]
@@ -98,11 +92,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             break
                     rooms.close()
                     if(controlForRoom == 0):   
-                        conn.sendall(b"HTTP/1.1 404 Not Found\n")
+                        response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                        conn.sendall(response)
                     else:
                         if(roomname == "" or int(day)>7 or int(day)<0 or int(hour)<9 or int(hour)+duration-1>17): #invalid input check
                             print("invalid input 400 atilcak")
-                            conn.sendall(b"HTTP/1.1 400 Bad Request\n")
+                            
+                            response =responseMessageFormat(b"400 Bad Request",b"Invalid input")
+                            conn.sendall(response)
                         else:
                             isReserved = 0 #if room already reserved sets to 1
                             if os.path.exists('reservations.txt'):
@@ -126,15 +123,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                                 reservationFileRead.close()
                             if(isReserved == 1):
                                 print("already reserved 403 atilcak")
-                                conn.sendall(b"HTTP/1.1 403 Forbidden\n")
+                                response =responseMessageFormat(b"403 Forbidden",b"Room is already reserved")
+                                conn.sendall(response)
                             else:
-                                conn.sendall(
-                                    b"HTTP/1.1 200 OK\n" +
-                                    b"Content-Type: text/html\n" +
-                                    b"\n")
+                                response =responseMessageFormat(b"200 OK",b"Room is reserved")
+                                conn.sendall(response)
                             
                 else:
-                    conn.sendall(b"HTTP/1.1 404 Not Found\n")    
+                    response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                    conn.sendall(response)
             elif funcType == "/checkavailability":
                 if os.path.exists('rooms.txt'):
                     roomname = name.split("&")[0]
@@ -150,11 +147,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     rooms.close()
                     if(controlForRoom == 0):
                         #print("room not found 404 atilcak ROOM YOK ROOMS TXT DE")
-                        conn.sendall(b"HTTP/1.1 404 Not Found\n")
+                        response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                        conn.sendall(response)
+
                     else:
                         if(int(day)>7 or int(day)<0): #invalid input check
                            # print("invalid input for day number /// 400 atilcak")
-                            conn.sendall(b"HTTP/1.1 400 Bad Request\n")
+                            response =responseMessageFormat(b"400 Bad Request",b"Invalid input")
+                            conn.sendall(response)
                         else:
                             if os.path.exists('reservations.txt'):
                                 roomsFile = open("reservations.txt", "r")
@@ -173,13 +173,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
                                 for i in availableHours:
                                     availableHoursReturn += str(i) + " "
-
+                                '''
                                 conn.sendall(b"HTTP/1.1 200 OK\n" 
                                              b"Content-Type: text/html\n" 
                                              b"\n" + availableHoursReturn.encode()) 
+                                '''
                                 print("available hours: ", availableHours)
+
+                                response =responseMessageFormat(b"200 OK",availableHoursReturn.encode())
+                                conn.sendall(response)
+
+                                
                             else:
-                                conn.sendall(b"HTTP/1.1 400 Bad Request\n")
+                                response =responseMessageFormat(b"200 OK",b"Room is available")
+                                conn.sendall(response)
             elif funcType == "/get": #listavailability?room=roomname
                 print("-------get request to room server-------")
                 
@@ -196,13 +203,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         break
                 if isExist==False:
                     print("The room name is not exist")
-                    conn.sendall(b"HTTP/1.1 404 Not Found\n")
+                    response =responseMessageFormat(b"404 Not Found",b"Room name is not exist")
+                    conn.sendall(response)
                 else:
-                    conn.sendall(b"HTTP/1.1 200 OK\n" +
-                                 b"Content-Type: text/html\n" +
-                                 b"\n" +
-                                 b"Room name: " + name.encode() )
+                    response =responseMessageFormat(b"200 OK",b"Room is exist")
+                    conn.sendall(response)
             else:
-                conn.sendall(b"HTTP/1.1 400 Bad Request\n")
+                response =responseMessageFormat(b"400 Bad Request",b"Invalid input")
+                conn.sendall(response)
 
 
---
+++ splitOperations.py
@@ -0,0 +1,33 @@
+
+def splitURL(url)          :
+    
+    requestLine=url.split("\n")[0]     
+    funcType= requestLine.split("?")[0].split(" ")[1]
+    
+    # check the function type is /favicon.ico
+    if funcType == "/favicon.ico":
+        name = "favicon.ico"
+    else: 
+        name=requestLine.split(" ")[1].split("=")[1]    
+
+
+
+    if name[-1] == "~":
+        name = name[:-1]
+
+    return funcType,name
+
+
+def responseMessageFormat(statu,bodyMessage):
+
+
+    response  =  b"HTTP/1.1 "+statu+b"\n"
+    response +=  b"Content-Type: text/html\n\n"     
+    response +=  b"<html>"
+    response +=  b"<head><title>Response</title></head>"
+    response +=  b"<body>"+bodyMessage+b"</body>"                
+    response +=  b"</html>"
+    
+    return response
+
+    
